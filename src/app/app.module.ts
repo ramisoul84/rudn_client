74ddce32bbf138d16243335b0aa118af689b3ab3
ramis <111607770+ramisoul84@users.noreply.github.com>
2024-02-03 10:46:05 +0300

    initial app

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
